788b504e70bcfcaf2e5032d37e91f083cb9ea4a3
Christophe Carvalho Vilas-Boas <christophe.carvalhovilasboas@gmail.com>
2013-01-26 10:21:49 +0100

    FMNNetwork functions

--- FMNNetwork.js
+++ FMNNetwork.js
@@ -1,6 +1,6 @@
 var FMNNetwork = {
-  networks : Array(),
-  stations : Array(),
+  networks : new Array(),
+  stations : new Array(),
 
   getNetworks : function() {
     return ["métro parisien" , "métro tahitien"];
@@ -18,19 +18,31 @@ var FMNNetwork = {
     return [
         ["ligne 4" , "station 2"],
         ["ligne 4" , "station 3"],
-        ["ligne 5" , "station 4"]
+        ["ligne 5" , "station 4"],
+        ["ligne 5" , "station 5"],
+        ["ligne 6" , "station 6"]
     ];
   },
 
   addNetwork : function (network) {
-
+    if (this.networks.indexOf(network) == -1) {
+      this.networks.put(network);
+    }
   },
 
   addStations : function (network , stations ) {
+    var networkIndex = this.networks.indexOf(network);
+    if (networkIndex == -1) {
+      this.addNetwork(network);
+      networkIndex = this.networks.length - 1;
+    }
+    var networkToComplete = this.networks[networkIndex];
 
+    var existingStations = this.getStations(networkToComplete) || new Array();
+    var newNetworkStations = existingStations.concat(stations);
   },
 
   addStation : function (network , station) {
-
+    this.addStations(network, [station]);
   }
 };
