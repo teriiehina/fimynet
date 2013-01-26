d6280de2992c968ca0ed2a309b302b7f6aed8421
Peter MEUEL <peter@teriiehina.net>
2013-01-26 10:17:33 +0100

    Add the data structures for the graph

---
+++ FMNDataStructures.js
@@ -0,0 +1,38 @@
+var FMNGraph = {
+  nodes : Array(),
+  edges : Array(),
+
+  adjacent : function (node1 , node2) {
+
+  },
+
+  neighbors : function(node) {
+
+  },
+
+  addNode : function (name) {
+
+  },
+
+  deleteNode : function (name) {
+
+  },
+
+  addEdge : function (node1 , node2 , value) {
+
+  },
+
+  deleteEdge : function (node1, node2) {
+
+  }
+};
+
+var FMNNode = {
+  name : "",
+};
+
+var FMNEdge =  {
+  value : "",
+  node1 : "",
+  node2 : ""
+};
