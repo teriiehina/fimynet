f52966e44575c4e3b8c5441f03f7035818ebda12
Peter MEUEL <peter@teriiehina.net>
2013-01-26 09:49:27 +0100

    first draft for the main logic

---
+++ FMNNetwork.js
@@ -0,0 +1,32 @@
+var FMNNetwork = {
+  networks : Array(),
+  stations : Array(),
+
+  getNetworks : function() {
+    return ["métro parisien" , "métro tahitien"];
+  },
+
+  getStations : function(network) {
+    return ["station 1", "station 2", "station 3", "station 4", "station 5"];
+  },
+
+  search : function (network , departure , arrival , mode) {
+    return [
+        ["ligne 4" , "station 2"],
+        ["ligne 4" , "station 3"],
+        ["ligne 5" , "station 4"]
+    ];
+  },
+
+  addNetwork : function (network) {
+
+  },
+
+  addStations : function (network , stations ) {
+
+  },
+
+  addStation : function (network , station) {
+
+  }
+};
