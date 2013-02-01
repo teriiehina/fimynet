466e6e7152d8743cd14a5716271c38950acc1d68
Peter MEUEL <peter@teriiehina.net>
2013-02-01 12:09:45 +0100

    add a CSS for the lines colors

--- www/test/test.js
+++ www/test/test.js
@@ -1,7 +1,25 @@
-var assert = require("assert")
+var assert   = require("assert")
+var networsk = require("../js/app/FMNNetwork")
+var graphs   = require("../js/app/FMNDataStructures")
+var network  = require("../js/app/FMNDataStructures").FMNNetwork;
+
 describe('Array', function(){
   describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    })
+  })
+})
+
+describe('PitaGraph', function(){
+  describe('#init', function(){
+    it('should return -1 when the value is not present', function(){
+      
+      myFMN = new network();
+      myFMN.addStations('Métro parisien', paris.nodes);
+
+      var networks = myFMN.getNetworks();
       assert.equal(-1, [1,2,3].indexOf(5));
       assert.equal(-1, [1,2,3].indexOf(0));
     })
