1098732ecf54e846749237eaa06ada0fd3fd12ce
Peter MEUEL <peter@teriiehina.net>
2013-02-01 13:19:07 +0100

    add the 'install' button in the header

--- www/js/install-button.js
+++ www/js/install-button.js
@@ -1,41 +1,45 @@
 
 define(function(require) {
-    var install = require('install');
+  var install = require('install');
 
-    function update() {
-        var btn = document.getElementById('install-btn');
-        if(install.state == 'uninstalled') {
-            btn.style.display = 'block';
-        }
-        else if(install.state == 'installed' || install.state == 'unsupported') {
-            btn.style.display = 'none';
-        }
+  function update() {
+    var btn         = document.getElementById('install-btn');
+    var header_btn  = document.getElementById('header-install-btn');
+
+    if(install.state == 'uninstalled') {
+      btn.style.display         = 'block';
+      header_btn.style.display  = 'block';
     }
-
-    function init() {
-        var btn = document.getElementById('install-btn');
-        btn.addEventListener('click', function() {
-            install();
-        });
-
-        install.on('change', update);
-
-        install.on('error', function(e, err) {
-            // Feel free to customize this
-            alert('There was an error during installation.');
-        });
-
-        install.on('showiOSInstall', function() {
-            // Feel free to customize this
-            alert('To install, press the forward arrow in Safari ' +
-                  'and touch "Add to Home Screen"');
-        });
+    else if(install.state == 'installed' || install.state == 'unsupported') {
+      btn.style.display         = 'none';
+      header_btn.style.display  = 'none';
     }
+  }
 
-    if(!document.getElementById('install-btn')) {
-        document.addEventListener('DOMContentLoaded', init);
-    }
-    else {
-        init();
-    }
+  function init() {
+    var btn = document.getElementById('install-btn');
+    btn.addEventListener('click', function() {
+      install();
+    });
+
+    install.on('change', update);
+
+    install.on('error', function(e, err) {
+      // Feel free to customize this
+      alert('There was an error during installation.');
+    });
+
+    install.on('showiOSInstall', function() {
+      // Feel free to customize this
+      alert('To install, press the forward arrow in Safari ' +
+          'and touch "Add to Home Screen"');
+    });
+  }
+
+  if(!document.getElementById('install-btn')) {
+    document.addEventListener('DOMContentLoaded', init);
+  }
+  else {
+    init();
+  }
 });
